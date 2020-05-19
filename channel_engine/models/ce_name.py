27bc39c19a4988088b0a818a5922de118ab1ae39
Julien Laloux <julien.laloux@logicasoft.eu>
2020-05-19 09:56:55 +0200

    [18204] Remove submodules and add paid modules directly into customer repo

---
+++ channel_engine/models/ce_name.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+from odoo import models, fields,api,_
+from odoo.exceptions import ValidationError,Warning,UserError
+
+
+class ChannelName(models.Model):
+    _name = 'ce.name'
+    _description = "Channel Name"
+
+    name = fields.Char('Channel Name', help="Channel Name")
+
+    @api.constrains('name')
+    def _identify_same_channel_name(self):
+        for record in self:
+            obj = self.search([('name','=ilike',record.name),('id','!=',record.id)])
+            if obj:
+                raise Warning("There is another channel name with the same name: %s" % record.name)                
